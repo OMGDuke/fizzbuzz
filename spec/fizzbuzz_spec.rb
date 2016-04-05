bfb5532824030fea2f8155502471ac4571eda364
OMGDuke <lex1989@gmail.com>
2016-04-05 22:05:53 +0100

    passing tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,24 @@
+
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  nums = *(1..100)
+  nums.each do |i|
+    if i % 3 == 0 && i % 5 == 0
+      it 'returns "fizzbuzz" if a multiple 3 & 5' do
+        expect(fizzbuzz(i)).to eq "fizzbuzz"
+      end
+    elsif i % 3 == 0
+      it 'returns "fizz" if a multiple of 3' do
+        expect(fizzbuzz(i)).to eq "fizz"
+      end
+    elsif i % 5 == 0
+      it 'returns "fizz" if a multiple of 5' do
+        expect(fizzbuzz(i)).to eq "buzz"
+      end
+    else
+      it "returns #{i} if not a multiple" do
+        expect(fizzbuzz(i)).to eq i
+      end
+    end
   end
 end
